dd8ef60ccb38b53303bf989385b49f41ca7c03dd
Sigurgeir Gudmundsson <essgje@gmail.com>
2022-01-26 12:46:42 +0000

    initial commit

---
+++ app/api/src/main.ts
@@ -0,0 +1,12 @@
+import {NestFactory} from '@nestjs/core'
+import {AppModule} from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  let port = 3001
+  let prefix = '/tm-api'
+  app.setGlobalPrefix(prefix)
+  await app.listen(port)
+  console.log('service is running. http://localhost:' + port + prefix)
+}
+bootstrap()
