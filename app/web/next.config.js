5b369489b9ac3007821be160bf24b149f8dbcbf4
Maciej <maciej@viceversa.is>
2022-01-31 09:41:57 +0000

    adjust next config

--- app/web/next.config.js
+++ app/web/next.config.js
@@ -1,4 +1,13 @@
 const withTreat = require('next-treat')()
 const withTM = require('next-transpile-modules')(['@island.is/ui'])
 
-module.exports = withTreat(withTM())
+module.exports = withTreat(
+  withTM(nextConfig => {
+    return {
+      ...nextConfig,
+      reactStrictMode: true,
+      assetPrefix: '/tr-web',
+      basePath: '/tr-web',
+    }
+  }),
+)
