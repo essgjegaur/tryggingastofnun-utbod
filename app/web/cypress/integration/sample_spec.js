655fa2b2bcf9022d62cb73716c63ebcb3b734057
Sigurgeir Gudmundsson <essgje@gmail.com>
2022-02-01 21:04:13 +0000

    cypress fixing

--- app/web/cypress/integration/sample_spec.js
+++ app/web/cypress/integration/sample_spec.js
@@ -4,10 +4,10 @@ describe('Cypress auto test', () => {
     // cy.visit('http://localhost:3000/tr-web')
 
     // Type in phonenumber
-    cy.get('.tel').type('8552222')
+    cy.get('#mobileNumber').type('8552222')
 
     // Login
-    cy.get('form').click()
+    cy.contains('Auðkenna').click()
     
     // Check if login page contains 'Umsóknir'
     cy.contains('Umsóknir')
